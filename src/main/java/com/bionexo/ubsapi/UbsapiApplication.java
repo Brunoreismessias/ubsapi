b8ab569ae26d1cb5acf86263fc489866e946cfb6
Brunoreismessias <47762365+Brunoreismessias@users.noreply.github.com>
2019-02-21 21:42:34 -0300

    commit inicial do projeto

---
+++ src/main/java/com/bionexo/ubsapi/UbsapiApplication.java
@@ -0,0 +1,13 @@
+package com.bionexo.ubsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsapiApplication.class, args);
+	}
+
+}
