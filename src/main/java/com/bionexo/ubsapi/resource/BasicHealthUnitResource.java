2b4867f10012a9833e78dda0d51bc74f46831dc0
Bruno Messias <reisbruno@gmail.com>
2019-02-24 20:06:06 -0300

    criaçao do metodo de consulta

--- src/main/java/com/bionexo/ubsapi/resource/BasicHealthUnitResource.java
+++ src/main/java/com/bionexo/ubsapi/resource/BasicHealthUnitResource.java
@@ -2,12 +2,16 @@ package com.bionexo.ubsapi.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bionexo.ubsapi.dto.BasicHealthUnitDTO;
+import com.bionexo.ubsapi.dto.OriginDTO;
 import com.bionexo.ubsapi.service.BasicHealthUnitService;
 
 @RestController
@@ -21,4 +25,11 @@ public class BasicHealthUnitResource {
 	public List<BasicHealthUnitDTO> listAll() {
 		return basicHealthUnitService.listAll();
 	}
+	
+	@GetMapping("/findUnitNear")
+	public List<BasicHealthUnitDTO> finUnitNear(@RequestBody OriginDTO origin, HttpServletResponse response) {
+		return basicHealthUnitService.findUnitNear(origin);
+	}
+
+	
 }
--- src/main/java/com/bionexo/ubsapi/service/BasicHealthUnitService.java
+++ src/main/java/com/bionexo/ubsapi/service/BasicHealthUnitService.java
@@ -1,12 +1,16 @@
 package com.bionexo.ubsapi.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bionexo.ubsapi.calculation.Haversine;
 import com.bionexo.ubsapi.converter.BasicHealthUnitConverter;
 import com.bionexo.ubsapi.dto.BasicHealthUnitDTO;
+import com.bionexo.ubsapi.dto.OriginDTO;
+import com.bionexo.ubsapi.model.BasicHealthUnit;
 import com.bionexo.ubsapi.repository.BasicHealthUnitRepository;
 
 @Service
@@ -14,11 +18,22 @@ public class BasicHealthUnitService {
 
 	@Autowired
 	private BasicHealthUnitRepository basicHealthUnitRepository;
-	
+
 	@Autowired
 	private BasicHealthUnitConverter converter;
-	
+
 	public List<BasicHealthUnitDTO> listAll() {
 		return converter.convertListEntityToDTO(basicHealthUnitRepository.findAll());
+	}
+
+	public List<BasicHealthUnitDTO> findUnitNear(OriginDTO origin) {
+		List<BasicHealthUnitDTO> listUnitNear = new ArrayList<>();
+
+		for (BasicHealthUnit basicHealthUnit : basicHealthUnitRepository.findAll()) {
+			if (Haversine.calculate(origin, basicHealthUnit.getGeoCode()) <= origin.getDistance()) {
+				listUnitNear.add(converter.convertEntityToDTO(basicHealthUnit));
+			}
+		}
+		return listUnitNear;
 	}
 }
