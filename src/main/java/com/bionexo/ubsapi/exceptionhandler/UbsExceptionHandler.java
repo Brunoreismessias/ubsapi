9df9cb554c9601b6a959b7c9f7bc6ea663df66df
Bruno Messias <reisbruno@gmail.com>
2019-02-24 21:32:10 -0300

    inclusão handleExcption

---
+++ src/main/java/com/bionexo/ubsapi/exceptionhandler/UbsExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.bionexo.ubsapi.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class UbsExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		String messageUser = messageSource.getMessage("message.invalid", null, LocaleContextHolder.getLocale());
+		return handleExceptionInternal(ex, messageUser, headers, HttpStatus.BAD_REQUEST, request);
+	}
+}
