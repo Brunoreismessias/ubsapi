78df63efb7bf22e6d4cb8d334484be016876fb05
Bruno Messias <reisbruno@gmail.com>
2019-02-24 21:21:20 -0300

    inclusão de documentacao cityResource

--- src/main/java/com/bionexo/ubsapi/resource/CityResource.java
+++ src/main/java/com/bionexo/ubsapi/resource/CityResource.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bionexo.ubsapi.dto.BasicHealthUnitDTO;
 import com.bionexo.ubsapi.dto.CityDTO;
 import com.bionexo.ubsapi.service.CityService;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/city")
@@ -17,6 +20,7 @@ public class CityResource {
 	@Autowired
 	private CityService cityService;
 	
+	@ApiOperation(value="Returns a list with all citys", response=CityDTO.class)
 	@GetMapping
 	public List<CityDTO> listAll() {
 		return cityService.listAll();
